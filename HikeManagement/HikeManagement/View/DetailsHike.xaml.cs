f2ad9506dc473d88ab68ff80f4456c0c95f8c1fa
LongTran8102 <longtpp0801@gmail.com>
2023-10-25 15:29:25 +0700

    Xamarin app

---
+++ HikeManagement/HikeManagement/View/DetailsHike.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HikeManagement.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsHike : ContentPage
+    {
+        public DetailsHike(Model.HikeModel model)
+        {
+            InitializeComponent();
+            Title = "Details hike";
+            nameHike.Text = model.Name;
+            locationHike.Text = model.Location;
+            dateHike.Text = model.Date;
+            parkingAvaiHike.Text = model.Parking;
+            DesHike.Text = model.Description;
+            LengthHike.Text = model.Length;
+            LevelHike.Text = model.Level;
+            MemQuantityHike.Text = model.Quantity;
+            TypeHike.Text = model.TypeHike;
+
+        }
+    }
+}
